334ba90bdd23e6bb0e34485f972e4932137a209b
ARM <ashikurrahamanmolla028@gmail.com>
2023-07-23 17:15:32 +0530

    Carousal Hero Added

---
+++ src/components/HeroCarousal/HeroCarousal.jsx
@@ -0,0 +1,70 @@
+import HeroSlider from "react-slick";
+// Styles
+import "slick-carousel/slick/slick.css"; // Update this line
+import "slick-carousel/slick/slick-theme.css"; // Update this line
+
+// Rest of the code remains the same...
+
+const HeroCarousal = () => {
+  const settingsSm = {
+    arrow: true,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  const settingsLg = {
+    arrow: true,
+    autoplay: true, // Correct prop name is 'autoplay'
+    centerMode: true,
+    centerPadding: "300px",
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  const images = [
+    "https://images.unsplash.com/photo-1505832018823-50331d70d237?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1208&q=80",
+    "https://images.unsplash.com/photo-1501785888041-af3ef285b470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+    "https://images.unsplash.com/photo-1499678329028-101435549a4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+    "https://images.unsplash.com/photo-1470770841072-f978cf4d019e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+    "https://images.unsplash.com/photo-1518098268026-4e89f1a2cd8e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80",
+  ];
+  return (
+    <>
+      <div className="hidden lg:block">
+        <HeroSlider {...settingsLg}>
+          {/* Logic */}
+          {images.map((image) => (
+            <div className="w-full h-96 px-2 py-3">
+              <img
+                src={image}
+                alt="Testing"
+                className="w-full h-full rounded-md"
+              />
+            </div>
+          ))}
+        </HeroSlider>
+      </div>
+
+      <div className="lg:hidden">
+        <HeroSlider {...settingsSm}>
+          {/* Logic */}
+          {images.map((image) => (
+            <div className="w-full h-64 md:h-80 py-3">
+              <img
+                src={image}
+                alt="Testing"
+                className="w-full h-full rounded-md"
+              />
+            </div>
+          ))}
+        </HeroSlider>
+      </div>
+    </>
+  );
+};
+
+export default HeroCarousal;
--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,12 +1,14 @@
 // This is pascal case not camel case
 import { Route } from "react-router-dom";
 import Navbar from "../components/Navbar/NavbarComonent";
+import HeroCarousal from "../components/HeroCarousal/HeroCarousal";
 
 
 const DefaultLayout = (props) => {
   return (
     <>
       <Navbar/>
+      <HeroCarousal/>
       {props.children}
     </>
   );
